64aadb3cb750e8c2127ccad8707c3019d4ebdc10
minq9 <mingyu340@gmail.com>
2023-10-06 18:03:29 +0900

    삭제 작성 완료, 디테일화면 작성중

--- src/main/java/com/example/gongu/mapper/NoteMapper.java
+++ src/main/java/com/example/gongu/mapper/NoteMapper.java
@@ -9,18 +9,23 @@ import java.util.List;
 
 @Mapper
 public interface NoteMapper {
+//    쪽지쓰기
+    void insertNote(NoteVo noteVo);
 
 //    보낸쪽지함
     List<NoteVo> selectSendAll(NoteCriteria noteCriteria);
 
     int selectSendTotal(Long senderNumber);
 
-    void updateSendLevel(Long senderNumber);
+    void updateSendLevel(NoteVo noteVo);
+
+//    보낸쪽지
+    NoteVo selectSendNote(Long noteNumber);
 
 //    받은쪽지함
     List<NoteVo> selectReceiveAll(NoteCriteria noteCriteria);
 
     int selectReceiveTotal(Long recieverNumber);
 
-    void updateReceiveLevel(Long recieverNumber);
+    void updateReceiveLevel(NoteVo noteVo);
 }
